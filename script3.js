03229ee347dbd8fb6033bd71953a460474f015e3
DAR47 <53145249+DAR47@users.noreply.github.com>
2025-05-19 04:57:51 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,9 +1,9 @@
 document.querySelector("#choose1").addEventListener("click", function () {
     document.cookie = "Проспект 40 лет Октября, 91"
 });
-document.querySelector("#choose1").addEventListener("click", function () {
+document.querySelector("#choose2").addEventListener("click", function () {
     document.cookie = "Проспект Кирова, 37В"
 });
-document.querySelector("#choose1").addEventListener("click", function () {
+document.querySelector("#choose3").addEventListener("click", function () {
     document.cookie = "Орджоникидзе, 1"
 });
