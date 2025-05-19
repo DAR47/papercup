cc795141d3107f74b5f9866534aee3972b5f7be6
DAR47 <53145249+DAR47@users.noreply.github.com>
2025-05-19 04:14:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ adress2 = document.querySelector("#adress2").textContent;
 adress3 = document.querySelector("#adress3").textContent;
 adress = "0";
 
-
+document.querySelector("#block1").addEventListener("click", function () {
+    document.cookie = "Проспект 40 лет Октября, 91";
+  });
 document.querySelector("#block2").addEventListener("click", function () {
-    document.cookie = "фывфыв";
+    document.cookie = "Проспект Кирова, 37В";
+  });
+document.querySelector("#block3").addEventListener("click", function () {
+    document.cookie = "Орджоникидзе, 1";
   });
