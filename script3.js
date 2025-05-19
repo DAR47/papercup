a4cdc45a366c66dd4dbbe8d64e3a158e27a740ea
DAR47 <53145249+DAR47@users.noreply.github.com>
2025-05-19 04:51:10 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,9 @@
+document.querySelector("#choose1").addEventListener("click", function () {
+    document.cookie = "Проспект 40 лет Октября, 91"
+});
+document.querySelector("#choose1").addEventListener("click", function () {
+    document.cookie = "Проспект Кирова, 37В"
+});
+document.querySelector("#choose1").addEventListener("click", function () {
+    document.cookie = "Орджоникидзе, 1"
+});
