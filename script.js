b2ce00000d7a599bfb93437ddd7cb446d54e3e99
DAR47 <53145249+DAR47@users.noreply.github.com>
2025-05-19 03:20:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+adress1 = document.querySelector("#adress1").textContent;
+adress2 = document.querySelector("#adress2").textContent;
+adress3 = document.querySelector("#adress3").textContent;
+adress = "0";
+
+document.cookie = "4";
+document.querySelector("#block2").addEventListener("click", function () {
+    
+  });
