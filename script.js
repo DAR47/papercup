95997ca56f3898a7d5de46e849ebcfce0f16a96d
DAR47 <53145249+DAR47@users.noreply.github.com>
2025-05-19 03:59:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ adress2 = document.querySelector("#adress2").textContent;
 adress3 = document.querySelector("#adress3").textContent;
 adress = "0";
 
-document.cookie = "4";
+
 document.querySelector("#block2").addEventListener("click", function () {
-    
+    document.cookie = adress2;
   });
